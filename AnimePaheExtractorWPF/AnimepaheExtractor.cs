c6ef1a190533c080ea598053c8d295ea56d51bfc
ZReC <thezrec@gmail.com>
2020-03-16 22:38:14 -0300

    Puppeteer package added

--- AnimePaheExtractorWPF/AnimepaheExtractor.cs
+++ AnimePaheExtractorWPF/AnimepaheExtractor.cs
@@ -1,31 +1,32 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-using System.Text.Json;
-using System.Net;
+﻿using System.Net;
 using System.IO;
 using System.Threading.Tasks;
+using Newtonsoft.Json;
+using System.Collections.Generic;
 
 namespace AnimePaheExtractorWPF {
     class AnimepaheExtractor {
-        public static string SearchResults;
+        //public static string SearchResults;
 
         public static async void Search(string query) {
             query = query.Length > 0 ? query : "overlord";
 
-            string url = "https://animepahe.com/api?m=search&l=8&q=" + query.Substring(0, query.Length>32 ? 32 : query.Length);
+            string url = "https://animepahe.com/api?m=search&l=8&q=" + query.Substring(0, query.Length > 32 ? 32 : query.Length);
 
             HttpWebRequest request = (HttpWebRequest)WebRequest.Create(url);
             request.AutomaticDecompression = DecompressionMethods.GZip | DecompressionMethods.Deflate;
 
             using (HttpWebResponse response = (HttpWebResponse)await request.GetResponseAsync())
             using (Stream stream = response.GetResponseStream())
-            using (StreamReader reader = new StreamReader(stream)) {
-                 = await reader.ReadToEndAsync();
-
-                JsonSerializer.Deserialize
-            }
+                using (StreamReader reader = new StreamReader(stream)) {
+                    string _json = await reader.ReadToEndAsync();
+                    SearchResults _jsonob = JsonConvert.DeserializeObject<SearchResults>(_json);
+                }
         }
 
+        class SearchResults {
+            public int Total { get; set; }
+            public IList<Dictionary<string, string>> Data { get; set; }
+        }
     }
 }
